241284b0a35c73c9de43d1d01d2b025f0c1c2066
GardenWren <garden.paquin@gmail.com>
2017-03-23 01:13:07 -0400

    got about blurb from json working

--- js/app.js
+++ js/app.js
@@ -3,19 +3,19 @@ var portfolioData = 'https://raw.githubusercontent.com/GardenWren/FEWD-Project/m
 
 
 //load data
-$.ajax(portfolioData).done(function(data) {
-    console.log(data.aboutBlurb);
+$.ajax(portfolioData).done(function(data) { 
+    data = JSON.parse(data);
     //Find portfolio section in index.html
     var portfolioContainer = $('#portfolio');
     //Find about section in index.html
     var aboutTextContainer = $('#abouttext');
     //add about text to about section
-    aboutTextContainer.append(data.about);
+    aboutTextContainer.append(data.aboutBlurb);
  
 });
 
 
 //May provide this part.
-function buildPortfolio(section) {
+function buildPortfolioSection(sectionData) {
  
 }
