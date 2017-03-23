b9d4c291448d66cbc039ee60516c3fc9b118bf1b
GardenWren <garden.paquin@gmail.com>
2017-03-23 00:05:38 -0400

    added some data

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// Store location of data
+var portfolioData = 'img/portfolio.json';
+
+
+//load data
+$.ajax(portfolioData).done(function(data) {
+    console.log(data);
+    //Find portfolio section in index.html
+    var portfolioContainer = $('#portfolio');
+    //Find about section in index.html
+    var aboutTextContainer = $('#abouttext');
+    //add about text to about section
+    aboutTextContainer.append(data.about);
+ 
+});
+
+
+//May provide this part.
+function buildPortfolio(section) {
+ 
+}
