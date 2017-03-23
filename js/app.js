2ef443cbc7bb574c493dc6e789ae7aac0abceaca
GardenWren <garden.paquin@gmail.com>
2017-03-23 01:06:35 -0400

    update json

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ var portfolioData = 'https://raw.githubusercontent.com/GardenWren/FEWD-Project/m
 
 //load data
 $.ajax(portfolioData).done(function(data) {
-    console.log(data.about.toString());
+    console.log(data.aboutBlurb);
     //Find portfolio section in index.html
     var portfolioContainer = $('#portfolio');
     //Find about section in index.html
